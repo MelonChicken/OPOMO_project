09217abd543285cad56f2d2c5cb3c9f416d7fada
MelonChicken <kimjw0724@gmail.com>
2022-11-17 22:49:24 +0900

    collapsible direction changed

--- js/collapsible.js
+++ js/collapsible.js
@@ -1,22 +1,5 @@
 function collapseUpDown(element) {
-  var before = document.getElementsByClassName("active")[0]; // 기존에 활성화된 버튼
-  if (before && document.getElementsByClassName("active")[0] != element) {
-    // 자신 이외에 이미 활성화된 버튼이 있으면
-    before.nextElementSibling.style.maxHeight = null; // 기존에 펼쳐진 내용 접고
-    before.classList.remove("active"); // 버튼 비활성화
-  }
-  element.classList.toggle("active"); // 활성화 여부 toggle
-
-  var content = element.nextElementSibling;
-  if (content.style.maxHeight != 0) {
-    // 버튼 다음 요소가 펼쳐져 있으면
-    content.style.maxHeight = null; // 접기
-  } else {
-    content.style.maxHeight = 5 + "%"; // 접혀있는 경우 펼치기
-  }
-}
-function collapseRightLeft(element) {
-  var before = document.getElementsByClassName("active")[0]; // 기존에 활성화된 버튼
+  let before = document.getElementsByClassName("active")[0]; // 기존에 활성화된 버튼
   if (before && document.getElementsByClassName("active")[0] != element) {
     // 자신 이외에 이미 활성화된 버튼이 있으면
     before.nextElementSibling.style.maxwidth = null; // 기존에 펼쳐진 내용 접고
@@ -24,11 +7,28 @@ function collapseRightLeft(element) {
   }
   element.classList.toggle("active"); // 활성화 여부 toggle
 
-  var content = element.nextElementSibling;
-  if (content.style.maxHeight != 0) {
+  let content = element.nextElementSibling;
+  if (content.style.maxwidth != 0) {
     // 버튼 다음 요소가 펼쳐져 있으면
-    content.style.maxHeight = null; // 접기
+    content.style.maxwidth = null; // 접기
   } else {
-    content.style.maxHeight = 100 + "%"; // 접혀있는 경우 펼치기
+    content.style.maxwidth = 5 + "%"; // 접혀있는 경우 펼치기
+  }
+}
+function collapseRightLeft(element) {
+  let before = document.getElementsByClassName("active")[0]; // 기존에 활성화된 버튼
+  if (before && document.getElementsByClassName("active")[0] != element) {
+    // 자신 이외에 이미 활성화된 버튼이 있으면
+    before.nextElementSibling.style.maxwidth = null; // 기존에 펼쳐진 내용 접고
+    before.classList.remove("active"); // 버튼 비활성화
+  }
+  element.classList.toggle("active"); // 활성화 여부 toggle
+
+  let child = document.querySelector(".child");
+  if (child.style.maxWidth != 0) {
+    // 버튼 다음 요소가 펼쳐져 있으면
+    child.style.maxWidth = null; // 접기
+  } else {
+    child.style.maxWidth = 100 + "%"; // 접혀있는 경우 펼치기
   }
 }
