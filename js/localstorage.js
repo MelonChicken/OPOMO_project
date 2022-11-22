d5b085f9dac10ea49452faa735c1fb5b2cf908dc
J_UNU <m.l02n23@gmail.com>
2022-11-22 21:26:53 +0900

    [update] 반응형 페이지 색깔 표시 삭제

--- js/localstorage.js
+++ js/localstorage.js
@@ -6,5 +6,5 @@ const savedPicture = localStorage.getItem("1");
 const container = document.querySelector(".exportPage__picContainer");
 
 container.innerHTML = `
-    <img class="pic" src='${savedPicture}' />
+    <img class="finalStep" src='${savedPicture}' />
   `;
