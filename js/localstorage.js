173767522e87cd9d256e8c57f9a66d59f074d397
J_UNU <m.l02n23@gmail.com>
2022-11-22 23:57:40 +0900

    [update] about customPage dragstart event 발생시 프레임(드롭 대상)의 z-index를 높여 인식되게끔 설정(추후 모바일의 DnD 구현이후 작동 잘 되는지 확인할 예정)

--- js/localstorage.js
+++ js/localstorage.js
@@ -1,10 +1,6 @@
-if (localStorage.getItem(element.id)) {
-  const lastData = localStorage.getItem(element.id);
-}
-
-// const savedPicture = localStorage.getItem("1");
+const savedPicture = localStorage.getItem("1");
 const container = document.querySelector(".exportPage__picContainer");
 
 container.innerHTML = `
-    <img class="pic" src='${savedPicture}' />
+    <img class="finalStep" src='${savedPicture}' />
   `;
