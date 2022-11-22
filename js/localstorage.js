94288f2a4f9317a76c00e0ab1463d1a19f968c52
J_UNU <m.l02n23@gmail.com>
2022-11-22 21:15:30 +0900

    [update] image-processing and basicExport 1. 패트와 매트 4:3 비율로 편집 완료 2. basicExport localstorage.js 예시 저장 완료

--- js/localstorage.js
+++ js/localstorage.js
@@ -1,3 +1,10 @@
-if (localStorage.getItem(element.id)) {
-  const lastData = localStorage.getItem(element.id);
-}
+// if (localStorage.getItem(element.id)) {
+//   const lastData = localStorage.getItem(element.id);
+// }
+
+const savedPicture = localStorage.getItem("1");
+const container = document.querySelector(".exportPage__picContainer");
+
+container.innerHTML = `
+    <img class="pic" src='${savedPicture}' />
+  `;
