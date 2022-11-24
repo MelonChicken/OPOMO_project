d9e205b6f6ad348c4577ef1cf4486df3ee606c61
J_UNU <m.l02n23@gmail.com>
2022-11-24 18:07:32 +0900

    [update] 1. customPage 모바일 DnD 시 접다 펴기 구현하는 중.. 2. customExportPage 최종 프레임 크기 조절 완료

--- js/collapsible.js
+++ js/collapsible.js
@@ -15,16 +15,19 @@ function collapseUpDown(element) {
     content.style.maxHeight = 5 + "%"; // 접혀있는 경우 펼치기
   }
 }
-
+//-------------------------------------------------------------------------------------------------------------//
+//-------------------------------------------------------------------------------------------------------------//
+//-------------------------------------------------------------------------------------------------------------//
 function collapseRightLeftS(element) {
   let before = document.getElementsByClassName("active")[0]; // 기존에 활성화된 버튼
   if (before && document.getElementsByClassName("active")[0] != element) {
     // 자신 이외에 이미 활성화된 버튼이 있으면
-    console.log(document.querySelector(".customPage__navMenu"));
+
     document.querySelector(".child01").style.maxWidth = null; // 기존에 펼쳐진 내용 접고
 
     before.classList.remove("active"); // 버튼 비활성화
   }
+
   element.classList.toggle("active"); // 활성화 여부 toggle
 
   let child = document.querySelector(".child01");
@@ -41,6 +44,9 @@ function collapseRightLeftS(element) {
   }
 }
 
+//-------------------------------------------------------------------------------------------------------------//
+//-------------------------------------------------------------------------------------------------------------//
+//-------------------------------------------------------------------------------------------------------------//
 function collapseRightLeftW(element) {
   let before = document.getElementsByClassName("active")[0]; // 기존에 활성화된 버튼
   if (before && document.getElementsByClassName("active")[0] != element) {
