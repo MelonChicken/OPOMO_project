bc2458d941aec2694912787b6eb6e60a057b5360
TPRPT <pinia124@naver.com>
2022-11-25 08:50:46 +0900

    모바일

--- js/mobileDetector.js
+++ js/mobileDetector.js
@@ -26,25 +26,35 @@ function noticeTouch(e) {
   let wishNav = document.querySelector(".customPage__wishList__nav");
 
   //nav가 active 된 경우에만 다음으로 진행한다.
-  if (true && (searchNav.classList.contains("active") || wishNav.classList.contains("active"))) {
+  if (
+    true &&
+    (searchNav.classList.contains("active") ||
+      wishNav.classList.contains("active"))
+  ) {
     let nowActivated = document.querySelector(".active");
-
+    let frame = document.querySelector(
+      ".customPage__myPoseContainerCover__picBox"
+    );
     //닫아야하는 커튼(nowOpened)이랑 커튼 박스(nowMenu) 확인
     let nowMenu = nowActivated.nextElementSibling;
     let nowOpened = nowMenu.getElementsByTagName("div")[0];
 
-
     // 이미지에 터치 이벤트 발생시
-    $('.publicPictureBox').on("touchstart", '.posePictureBox', function(event) {
-      //커튼을 닫고 박스를 치운다.
-        console.log('커튼 닫어!')
-        console.log(e.target, '터치 시작되는 놈')
-        nowOpened.style.maxWidth = null;
-        nowMenu.style.width = null;
-        nowMenu.style.display = 'none';
-    });
+    $(".posePictureBox").on("touchstart", function (event) {
+        //커튼을 닫고 박스를 치운다.
+        console.log("커튼닫기");
+        console.log(e.target, '최초 터치 대상')
+        setTimeout(
+          () => (nowOpened.style.maxWidth = null),
+          (nowMenu.style.width = null),
+          (nowMenu.display = 'none'),
+          console.log("2초 후에 실행됨"),
+          2000
+        );
+      }
+    );
 
-    $('.posePictureBox').on("touchend", function(event) {
+    $('.customPage__myPoseContainerCover__picBox__grid__pic ui-sortable-handle').on("touchend", function(event) {
       //박스를 다시 꺼내고 커튼도 연다.
       //customPage__myPoseContainerCover__picBox__grid__pic ui-sortable-handle
       console.log('커튼 열어!!')
