28b4b9bea257d6cf053eb1810787f2a4063482c1
no-glass-otacku <ga7262681@gmail.com>
2022-11-25 01:10:52 +0900

    firebase js 위치 맨위로 옮김

--- js/searchAmove.js
+++ js/searchAmove.js
@@ -1,3 +1,22 @@
+//js파일 안에 js파일 넣음
+document.write(
+  '<script src="https://www.gstatic.com/firebasejs/8.8.1/firebase-app.js"></script>'
+);
+document.write(
+  '<script src="https://www.gstatic.com/firebasejs/8.8.1/firebase-database.js"></script>'
+);
+document.write(
+  '<script src="https://www.gstatic.com/firebasejs/8.8.1/firebase-analytics.js"></script>'
+);
+document.write(
+  '<script src="https://www.gstatic.com/firebasejs/8.8.1/firebase-auth.js"></script>'
+);
+document.write(
+  '<script src="https://www.gstatic.com/firebasejs/8.8.1/firebase-firestore.js"></script>'
+);
+document.write(
+  '<script type="module"> import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";</script>'
+);
 //이미지
 document.write('<script src="./js/firebaseUpload.js"></script>');
 const sibal = getWhichData("poses", "basic");
@@ -67,23 +86,3 @@ function saving(element) {
 function moveExport() {
   location.href = "/basicExport.html";
 }
-
-//js파일 안에 js파일 넣음
-document.write(
-  '<script src="https://www.gstatic.com/firebasejs/8.8.1/firebase-app.js"></script>'
-);
-document.write(
-  '<script src="https://www.gstatic.com/firebasejs/8.8.1/firebase-database.js"></script>'
-);
-document.write(
-  '<script src="https://www.gstatic.com/firebasejs/8.8.1/firebase-analytics.js"></script>'
-);
-document.write(
-  '<script src="https://www.gstatic.com/firebasejs/8.8.1/firebase-auth.js"></script>'
-);
-document.write(
-  '<script src="https://www.gstatic.com/firebasejs/8.8.1/firebase-firestore.js"></script>'
-);
-document.write(
-  '<script type="module"> import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";</script>'
-);
